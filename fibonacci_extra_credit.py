ce6955efb780581225d29c16cef32fd6fd429bcb
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-06-18 14:45:04 -0700

    Added comment

--- fibonacci_extra_credit.py
+++ fibonacci_extra_credit.py
@@ -1,6 +1,7 @@
 def fib_iterative(n):
   #Our previous code just pushed either 0 or 1 to the list, but not both, so it was running out of list items to add. Better just to initialize the list as [0,1]
   result = [0,1] 
+  #Will not run if n = 1 or 0
   for i in range(2, n+1):
     # I think our previous code might also have added result[n-1] and result[n-2] instead of i-1 and i-2 as well
     result.append(result[i-1] + result[i-2])
